c273b73947ae5d5c6f4880c5038a03a182f6d783
AJ Klopfenstein <aj.klopfenstein@gmail.com>
2015-02-24 11:31:55 -0600

    Testing the commit
    I added a pair of comments into CreditsScreen to test the functionality of GitGui and GitHub

--- Assets/Scripts/CreditsScreen.cs
+++ Assets/Scripts/CreditsScreen.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class CreditsScreen : MonoBehaviour {
 
+    //I am adding a comment to test GIT integration
+    //More testing is being made
+
 	// Use this for initialization
 	void Start () 
     {
